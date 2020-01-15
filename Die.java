8f17d79028463e27a6a01c185c4e481ee6013977
Yumi Balthazar <yumiann30@gmail.com>
2020-01-15 08:55:08 -0800

    Working die class

--- Die.java
+++ Die.java
@@ -2,13 +2,13 @@
 /**
  * Write a description of class Die here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Yumi Balthazar
+ * @version 2020-01-15
  */
 public class Die
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private int result;
 
     /**
      * Constructor for objects of class Die
@@ -16,18 +16,24 @@ public class Die
     public Die()
     {
         // initialise instance variables
-        x = 0;
+        result = roll();
     }
 
     /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
+     * Rolls the die and returns the result
      */
-    public int sampleMethod(int y)
+    public int roll()
     {
         // put your code here
-        return x + y;
+        result = (int) (Math.random() * 6 + 1);
+        return result;
+    }
+
+    /**
+     * Gets the result of the die
+     */
+    public int getResult()
+    {
+        return result;
     }
 }
