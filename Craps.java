e03a0315ee4ee933290ae3ab5d41af4a5a756d21
Yumi Balthazar <yumiann30@gmail.com>
2020-01-15 09:34:58 -0800

    We have a working game

--- Craps.java
+++ Craps.java
@@ -28,9 +28,21 @@ public class Craps
         }
     }
     
+    public static int rollTheDice(Scanner in, Die d)
+    {
+        System.out.print("Press <Enter> to roll...");
+        in.nextLine();       // Don't even need to save their input
+        int result1 = d.roll();
+        int result2 = d.roll();
+        int total = result1 + result2;
+        System.out.println("You rolled a " + result1 + " and a " + result2 + " for a total of " + total);
+        return total;
+    }
+    
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
+        Die d = new Die();
         System.out.println("The Game of Craps!");
         giveInstructions(in);
         System.out.println("Let's play craps!");
@@ -38,23 +50,45 @@ public class Craps
         String playStatus = "y";
         while(playStatus.substring(0,1).equalsIgnoreCase("y"))
         {
-            System.out.println("DEBUG: We're going to play Craps...");
+            System.out.println("First roll!");
+            int result = rollTheDice(in,d);
+            if (result == 7 || result == 11)
+            {
+                System.out.println("You win");
+            }
+            else if ( result == 2 || result == 3 || result == 12)
+            {
+                System.out.println("You lose");
+            }
+            else
+            {
+                int point = result;
+                System.out.println("Your point is " + point + ". Roll again to try and get that value!");
+                result = rollTheDice(in,d);
+                while (result != 7 && result != point)
+                {
+                    System.out.println("Roll again!");
+                    result = rollTheDice(in,d);
+                    
+                }
+                if (result == 7)
+                {
+                    System.out.println("So sorry! You lose!");
+                }
+                else
+                {
+                    System.out.println("You win!");
+                }
+            }
                         
-            System.out.print("Press <Enter> to roll...");
-            String pause = in.nextLine();
-            
-            
-            
-            
+
             System.out.println("Would you like to play again? (Y/n)");
             playStatus = in.nextLine();
             if (playStatus.equals("")) { playStatus = "y";}
-            
-            
-            
-            
-            
+
         }
+        
+        System.out.print("Thanks for playing the game!");
         
     }
 }
